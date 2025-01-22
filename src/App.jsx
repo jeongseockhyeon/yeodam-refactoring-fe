fd091134053f3a2f5933c1e02fca8cf0215c3a4c
jeongseokhyeon <jeong9235@gmail.com>
2025-01-23 00:01:54 +0900

    Feat:로그인 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import UserJoin from './pages/UserJoin'
+import UserJoin from './pages/user/UserJoin'
+import Login from './pages/auth/login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/users/join' element={<UserJoin/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
       </Routes>
     </BrowserRouter>
   )
